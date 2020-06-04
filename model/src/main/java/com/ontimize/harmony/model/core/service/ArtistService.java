9d37ee9ced9a590ae1583c2f29657d06c6a8154a
manuelsrleon <manuel.santamarina@campusdual.com>
2020-06-04 16:30:20 +0200

    artist DAO

---
+++ model/src/main/java/com/ontimize/harmony/model/core/service/ArtistService.java
@@ -0,0 +1,47 @@
+package com.ontimize.harmony.model.core.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import com.ontimize.db.EntityResult;
+import com.ontimize.harmony.api.core.service.IArtistService;
+import com.ontimize.harmony.model.core.dao.ArtistDao;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+
+@Service("ArtistService")
+@Lazy
+public class ArtistService implements IArtistService {
+
+	@Autowired private ArtistDao artistDao;
+	@Autowired private DefaultOntimizeDaoHelper daoHelper; 
+	@Override
+	
+	public EntityResult artistQuery(Map<String, Object> keyMap, List<String> attrList)
+			throws OntimizeJEERuntimeException {
+		// 
+		return this.daoHelper.query(this.artistDao, keyMap, attrList);
+	}
+	
+	@Override
+	public EntityResult artistInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException{
+		return this.daoHelper.insert(this.artistDao, attrMap);
+		}
+
+	@Override
+	public EntityResult artistUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+			throws OntimizeJEERuntimeException {
+		return this.daoHelper.update(this.artistDao, attrMap, keyMap);
+	}
+
+	@Override
+	public EntityResult artistDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+		
+		return this.daoHelper.delete(this.artistDao, keyMap);
+	}
+
+}
---
+++ ws/src/main/java/com/ontimize/harmony/ws/core/rest/ArtistRestController.java
@@ -0,0 +1,26 @@
+package com.ontimize.harmony.ws.core.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ontimize.harmony.api.core.service.IArtistService;
+import com.ontimize.jee.server.rest.ORestController;
+
+@RestController
+@RequestMapping("/artists") //TODO TEST
+@ComponentScan(basePackageClasses = {com.ontimize.harmony.api.core.service.IArtistService.class})
+public class ArtistRestController extends ORestController<IArtistService> {
+	
+	@Autowired
+	private IArtistService songService;
+	
+	
+	@Override
+	public IArtistService getService() {
+
+		return this.songService;
+	}
+
+}
